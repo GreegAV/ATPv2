86baba41ca94214ad4e4e22e27b84aa4975a4218
GreegAV <greegav@gmail.com>
2018-05-26 16:18:46 +0300

    Cleanup

--- src/main/java/dao/ConfigManager.java
+++ src/main/java/dao/ConfigManager.java
@@ -4,19 +4,18 @@ import java.util.ResourceBundle;
 
 public class ConfigManager {
 
-    public static final String DRIVER = "DRIVER";
-    public static final String URL = "URL";
-    public static final String USERNAME = "USERNAME";
-    public static final String PASSWORD = "PASSWORD";
-    public static final String DBNAME = "DBNAME";
-    public static final String CONNECTIONPARAMS = "CONNECTIONPARAMS";
+    static final String DRIVER = "DRIVER";
+    static final String URL = "URL";
+    static final String USERNAME = "USERNAME";
+    static final String PASSWORD = "PASSWORD";
+    static final String DBNAME = "DBNAME";
+    static final String CONNECTIONPARAMS = "CONNECTIONPARAMS";
 
     private static final String BUNDLE_NAME = "config";
     private static ConfigManager instance;
     private ResourceBundle resource;
 
     private ConfigManager() {
-
     }
 
     public static ConfigManager getInstance() {
@@ -27,7 +26,7 @@ public class ConfigManager {
         return instance;
     }
 
-    public String getProperty(String key) {
+    String getProperty(String key) {
         return (String) resource.getObject(key);
     }
 }
